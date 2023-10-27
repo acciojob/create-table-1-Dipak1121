e424393223102ca8c8c24dd3a56403da56081fe7
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-10-27 18:51:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+let table = document.getElementById("sampleTable");
 function insert_Row() {
     //Write your code here
-  
-  
+  let tr = document.createElement("tr");
+  let td1 = document.createElement("td");
+  let td2 = document.createElement("td");
+	td1.innerText = "New Cell1";
+	tr.appendChild(td1);
+	td2.innerText = "New Cell2";
+	tr.appendChild(td2);
+	let child = table.children;
+	table.insertBefore(tr, child[0]);
 }
